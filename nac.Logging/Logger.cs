3186fdd7e2f0db1d3e8fc62ae946178e1140b0ac
Nathaniel A Collier <drxs3v3n@gmail.com>
2023-12-06 07:42:53 -0600

    Add project files.

---
+++ nac.Logging/Logger.cs
@@ -0,0 +1,65 @@
+﻿using nac.Logging.model;
+using System;
+using System.Runtime.CompilerServices;
+
+namespace nac.Logging;
+
+public class Logger
+{
+    public static event EventHandler<LogMessage> OnNewMessage;
+
+    private string Caller_className;
+
+    public Logger()
+    {
+        // get calling assembly
+        var mth = new System.Diagnostics.StackTrace().GetFrame(1).GetMethod();
+        var _class_to_log = mth.ReflectedType;
+
+        this.Caller_className = _class_to_log.FullName;
+    }
+
+    public void Info(string message, [CallerMemberName] string callerMemberName = "")
+    {
+        OnNewMessage?.Invoke(this, new LogMessage
+        {
+            Level = "INFO",
+            Message = message,
+            CallingClassName = Caller_className,
+            CallingMemberName = callerMemberName
+        });
+    }
+
+    public void Debug(string message, [CallerMemberName] string callerMemberName = "")
+    {
+        OnNewMessage?.Invoke(this, new LogMessage
+        {
+            Level = "DEBUG",
+            Message = message,
+            CallingClassName = Caller_className,
+            CallingMemberName = callerMemberName
+        });
+    }
+
+    public void Warn(string message, [CallerMemberName] string callerMemberName = "")
+    {
+        OnNewMessage?.Invoke(this, new LogMessage
+        {
+            Level = "WARN",
+            Message = message,
+            CallingClassName = Caller_className,
+            CallingMemberName = callerMemberName
+        });
+    }
+
+    public void Error(string message, [CallerMemberName] string callerMemberName = "")
+    {
+        OnNewMessage?.Invoke(this, new LogMessage
+        {
+            Level = "ERROR",
+            Message = message,
+            CallingClassName = Caller_className,
+            CallingMemberName = callerMemberName
+        });
+    }
+}
---
+++ nac.Logging/model/LogMessage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace nac.Logging.model;
+
+public class LogMessage
+{
+    public string Level { get; set; }
+    public string Message { get; set; }
+    public string CallingMemberName { get; set; }
+    public DateTime EventDate { get; }
+    public string CallingClassName { get; internal set; }
+
+    public LogMessage()
+    {
+        this.EventDate = DateTime.Now;
+    }
+
+    public override string ToString()
+    {
+        return $"[{EventDate:yyyy-MM-dd hh:mm:ss tt}] - {Level} - {CallingClassName}.{CallingMemberName} - {Message}";
+    }
+}
